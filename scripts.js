a213d6f0994de6cb0efcaace1817716bf5d9547f
Iegor Stetsiura <biropka@gmail.com>
2019-03-04 12:24:14 +0200

    scroll listener added

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var sticky = document.getElementById('sticky-block');
+
+document.addEventListener('scroll', function () {
+    if(window.innerWidth >= 768) {
+        setStickyPosition(sticky);
+    }
+});
+
+
+function setStickyPosition(block) {
+
+}
+
+/*function setStickyPosition() {
+
+}*/
