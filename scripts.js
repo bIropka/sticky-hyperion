284aa34b61566f91609fdb8a72519a5dca37d71e
Iegor Stetsiura <biropka@gmail.com>
2019-03-04 14:38:02 +0200

    conditions added

--- scripts.js
+++ scripts.js
@@ -9,6 +9,20 @@ document.addEventListener('scroll', function () {
 
 function setStickyPosition(block) {
 
+    var parent = block.parentNode,
+        parentTop = parent.getBoundingClientRect().top,
+        parentHeight = parent.offsetHeight,
+        stickyHeight = block.offsetHeight;
+
+
+    if (parentTop + parentHeight - stickyHeight <= 0) {
+        console.log('bottom');
+    } else if (parentTop <= 0) {
+        console.log('sticky');
+    } else {
+        console.log('top');
+    }
+
 }
 
 /*function setStickyPosition() {
